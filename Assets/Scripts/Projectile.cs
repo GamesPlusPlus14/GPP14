d1da588cfe9dfcf822cf529d8cac02df08dd1151
Fib <pixelmonium@gmail.com>
2014-03-01 11:49:43 -0600

    Spawning & Projectiles Code

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public GameObject ProjPrefab;
+	public float Speed;
+	Vector3 direction;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+
+		}
+	}
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject PrefabToSpawn;
+	public float MinSpawnTimer;
+	public float MaxSpawnTimer;
+
+	float spawnTimer;
+	float timer;
+
+	// Use this for initialization
+	void Start () 
+	{
+		spawnTimer = Random.Range(MinSpawnTimer, MaxSpawnTimer);
+		// Debugging (check if min is greater than max)
+		if (MinSpawnTimer > MaxSpawnTimer)
+			Debug.Log(gameObject.name + ": The minimum spawn time is greater than the maximum spawn timer.");
+		// End debugging
+
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer += Time.deltaTime;
+
+		if (timer >= spawnTimer)
+		{
+			Instantiate(PrefabToSpawn);
+			timer = 0;
+		}
+
+	}
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawIcon(transform.position, "spawnGizmo.png");
+	}
+}
