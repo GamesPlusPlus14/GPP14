c8ef369629f33f8c214495c717f46defa544f5e4
Fib <pixelmonium@gmail.com>
2014-03-01 13:46:20 -0600

    ProjectilePrefabs & inventory

---
+++ Assets/Scripts/ItemTarget.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemTarget : MonoBehaviour {
+	
+	public GameObject ItemToReceive;
+	Inventory inven;
+
+	// Use this for initialization
+	void Start () {
+		inven = GameObject.FindGameObjectWithTag("InventoryManager").GetComponent<Inventory>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			if (inven.inventory.Contains(ItemToReceive))
+			{
+				inven.RemoveItem(ItemToReceive);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pickup : MonoBehaviour {
+
+	Inventory inven;
+
+	// Use this for initialization
+	void Start () 
+	{
+		inven = GameObject.FindGameObjectWithTag("InventoryManager").GetComponent<Inventory>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Pickup")
+		{
+			inven.AddItem(other.gameObject);
+			Destroy(other.gameObject);
+		}
+	}
+}
