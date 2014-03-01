60026d39fe9d0e5770006a0c7bfeab84be98edc2
coalczar <josh.koleszar@gmail.com>
2014-03-01 13:52:47 -0600

    Add meta

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () 
+    {
+        DontDestroyOnLoad(gameObject);
+        if (Application.loadedLevel == 0)
+            StartCoroutine(OneOrTwoPlayers());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+
+    IEnumerator OneOrTwoPlayers()
+    {
+        bool selected = false;
+        while (!selected)
+        {
+
+            yield return null;
+        }
+    }
+}
