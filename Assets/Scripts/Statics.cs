2870a699d5337ba2e11ff8c8bc7deb6ae860a0ce
coalczar <josh.koleszar@gmail.com>
2014-03-01 11:22:29 -0600

    Statics script

---
+++ Assets/Scripts/Statics.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+public class Statics 
+{
+    static readonly string PHorz = "PlayerHorizontal";
+    static readonly string PVert = "PlayerVertical";
+
+    static readonly string WHorz = "WindowHorizontal";
+    static readonly string WVert = "WindowVertical";
+
+    static readonly string WHorz1 = "WindowHorizontal1";
+    static readonly string WVert1 = "WindowVertical1";
+
+}
